afa91f6b895e657e47c7e8cea23e9130d449541a
Sergio Falcon <sergiojfalcon@gmail.com>
2021-02-20 02:41:22 -0600

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,27 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { auth } from '../../firebase/firebase.utils';
-import { ReactComponent as Logo } from '../../assets/crown.svg'; 
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
-        <div className='first'> 
-            <Link className='logo-container' to='/'>
-                <Logo className='logo' />
-            </Link>
-            {
-                currentUser ?
-                (<h1 className='welcome-text'>Welcome {currentUser.displayName}</h1>)
-                :
-                null
-            }
-        </div>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
         <div className='options'>
             <Link className='option' to='/shop'>
                 SHOP
@@ -46,9 +41,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
